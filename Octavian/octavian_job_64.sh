e17c2a3674d1061879dd342c10cdde5680aa8aee
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-07-13 14:45:18 +0200

    init

---
+++ Octavian/octavian_job_64.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 128
+#SBATCH -A pc2-mitarbeiter
+#SBATCH -o octavian_job_64.out
+#SBATCH -J octavian_job_64
+#SBATCH -p all
+#SBATCH -t 30:00
+#SBATCH --exclusive
+
+module reset
+
+export JULIA_NUM_THREADS=64 # 64 == single socket
+
+# Need to use Julia 1.8(!) which we don't yet have as a module
+/scratch/pc2-mitarbeiter/bauerc/.julia/juliaup/julia-1.8.0-rc1+0~x64/bin/julia --project dgemm_octavian.jl 10240
---
+++ OpenBLAS/openblas_job_64.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 128
+#SBATCH -A pc2-mitarbeiter
+#SBATCH -o openblas_job_64.out
+#SBATCH -J openblas_job_64
+#SBATCH -p all
+#SBATCH -t 30:00
+#SBATCH --exclusive
+
+module reset
+
+NTHREADS=64 # 64 == single socket
+export JULIA_NUM_THREADS=$NTHREADS
+export OMP_NUM_THREADS=$NTHREADS
+export MKL_DYNAMIC=false
+export OMP_PLACES=CORES
+export OMP_PROC_BIND=CLOSE
+
+# Need to use Julia 1.8(!) which we don't yet have as a module
+/scratch/pc2-mitarbeiter/bauerc/.julia/juliaup/julia-1.8.0-rc1+0~x64/bin/julia --project dgemm_openblas.jl 10240
